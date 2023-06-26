c69510f80706678ffa6df229e798e59e7164f243
reqkram <requillo.ma@gmail.com>
2023-06-26 21:50:34 +0800

    Modified f.

--- src/main/java/com/test/Main.java
+++ src/main/java/com/test/Main.java
@@ -4,20 +4,19 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.attribute.FileAttribute;
+import java.nio.file.attribute.PosixFilePermission;
+import java.nio.file.attribute.PosixFilePermissions;
+import java.util.Set;
 
 public class Main {
     final static String EP_FILE = "ep_file";
     public static void main(String[] args) throws IOException {
         try{
             String receiptNo = "test111";
-            File file = File.createTempFile(EP_FILE + receiptNo ,".pdf");
-            boolean isReadable = file.setReadable(true, true);
-            boolean isWritable =file.setWritable(true, true);
-            boolean isExecutable = file.setExecutable(true, true);
+            String suffix = ".json";
+            File file = File.createTempFile(receiptNo,suffix);
             System.err.println(file.getAbsolutePath());
-            System.err.println(isReadable);
-            System.err.println(isWritable);
-            System.err.println(isExecutable);
         }catch (Exception e){
             System.err.println(e.getMessage());
         }
