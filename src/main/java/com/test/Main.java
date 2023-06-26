8b99f8a0a2dabadb8dc08da7faa37655b353c4fe
reqkram <requillo.ma@gmail.com>
2023-06-26 21:55:13 +0800

    Removed unused code.

--- src/main/java/com/test/Main.java
+++ src/main/java/com/test/Main.java
@@ -2,20 +2,13 @@ package com.test;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.attribute.FileAttribute;
-import java.nio.file.attribute.PosixFilePermission;
-import java.nio.file.attribute.PosixFilePermissions;
-import java.util.Set;
 
 public class Main {
     final static String EP_FILE = "ep_file";
     public static void main(String[] args) throws IOException {
         try{
-            String receiptNo = "test111";
             String suffix = ".json";
-            File file = File.createTempFile(receiptNo,suffix);
+            File file = File.createTempFile(EP_FILE,suffix);
             System.err.println(file.getAbsolutePath());
         }catch (Exception e){
             System.err.println(e.getMessage());
