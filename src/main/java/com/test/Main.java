11dd47ac5d03a36b95cf0b1ddab69f7941811094
reqkram <requillo.ma@gmail.com>
2023-06-26 21:13:57 +0800

    Initial commit.

--- src/main/java/com/test/Main.java
+++ src/main/java/com/test/Main.java
@@ -3,13 +3,19 @@ package com.test;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Main {
     final static String EP_FILE = "ep_file";
     public static void main(String[] args) throws IOException {
         File file = null;
         String receiptNo = "test";
+
         file = Files.createTempFile(EP_FILE, receiptNo + ".pdf").toFile();
+
+        Path tempPath = Files.createTempDirectory("");
+        File tempDir = tempPath.toFile();
+        file = new File(tempDir, "ep_payment" + receiptNo + ".pdf");
         boolean isReadable = file.setReadable(true, true);
         boolean isWritable =file.setWritable(true, true);
         boolean isExecutable = file.setExecutable(true, true);
