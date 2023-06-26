50cbedfbe64a7ce064c42899ea9102108856491a
reqkram <requillo.ma@gmail.com>
2023-06-26 21:09:30 +0800

    Initial commit.

--- src/main/java/com/test/Main.java
+++ src/main/java/com/test/Main.java
@@ -7,7 +7,16 @@ import java.nio.file.Files;
 public class Main {
     final static String EP_FILE = "ep_file";
     public static void main(String[] args) throws IOException {
-        File file = Files.createTempFile(EP_FILE, "test" + ".pdf").toFile();
-        System.out.println(file.getAbsolutePath());
+        File file = null;
+        String receiptNo = "test";
+        file = Files.createTempFile(EP_FILE, receiptNo + ".pdf").toFile();
+        boolean isReadable = file.setReadable(true, true);
+        boolean isWritable =file.setWritable(true, true);
+        boolean isExecutable = file.setExecutable(true, true);
+        System.err.println(file.getAbsolutePath());
+        System.err.println(isReadable);
+        System.err.println(isWritable);
+        System.err.println(isExecutable);
+
     }
 }
