cd45b4a780737d31ee78300bb19ba7fb44255ec0
reqkram <requillo.ma@gmail.com>
2023-06-26 22:14:57 +0800

    Removed unused code.

--- src/main/java/com/test/Main.java
+++ src/main/java/com/test/Main.java
@@ -2,14 +2,22 @@ package com.test;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Main {
     final static String EP_FILE = "ep_file";
     public static void main(String[] args) throws IOException {
         try{
-            String suffix = ".json";
-            File file = File.createTempFile(EP_FILE,suffix);
+            String receiptNo = "test111";
+            File file = Files.createTempFile(EP_FILE + receiptNo ,".pdf").toFile();
+            boolean isReadable = file.setReadable(true, true);
+            boolean isWritable =file.setWritable(true, true);
+            boolean isExecutable = file.setExecutable(true, true);
             System.err.println(file.getAbsolutePath());
+            System.err.println(isReadable);
+            System.err.println(isWritable);
+            System.err.println(isExecutable);
         }catch (Exception e){
             System.err.println(e.getMessage());
         }
